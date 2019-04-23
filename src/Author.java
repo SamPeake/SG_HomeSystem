08520b95a85a6cf8f3a52d995b4d1a999afb7fa5
edavis19 <edavis19@vt.edu>
2019-04-23 15:47:26 -0400

    Basic Author object

--- src/Author.java
+++ src/Author.java
@@ -1,7 +1,37 @@
 public class Author {
+    private int authID;
+    private String name;
+    private String role;
+    private int phoneNumber;
 
-    public Author() {
-        // TODO Auto-generated constructor stub
+    public Author(int prevID, String inName, String inRole, int inPhoneNumber) {
+        authID = prevID + 1;
+        name = inName;
+        role = inRole;
+        phoneNumber = inPhoneNumber;
     }
 
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAuthID() {
+        return authID;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
